ec4c93e99974983a9d8243980caa544450a8d640
fangxingguang <fangxingguang@grandcloud.cn>
2017-07-20 10:23:03 +0800

    '优化页面'

---
+++ src/renderer/views/taskAuto/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Card>
+      <Tabs type="card">
+        <Tab-pane label="发布任务">
+          <add></add>
+        </Tab-pane>
+        <Tab-pane label="已发布任务">
+          <send></send>
+        </Tab-pane>
+        <Tab-pane label="我的任务">
+          <my></my>
+        </Tab-pane>
+      </Tabs>
+    </Card>
+  </div>
+</template>
+
+<script>
+import add from './add';
+import send from './send';
+import my from './my';
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    add, send, my
+  }
+}
+</script>
+
+<style scoped>
+.search-input {
+  float: right;
+}
+</style>
