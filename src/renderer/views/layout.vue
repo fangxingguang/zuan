672f4cfb0ac53b70d49ababdc3a4a7b7e9c43c3a
fangxingguang <fangxingguang@grandcloud.cn>
2017-07-21 20:36:00 +0800

    'TODO'

--- src/renderer/views/layout.vue
+++ src/renderer/views/layout.vue
@@ -1,4 +1,15 @@
 <style scoped>
+.layout-top-menu {
+  height: 30px;
+  padding-top: 5px;
+}
+
+.layout-main-menu {
+  display: block;
+}
+.layout-right-tools{
+  color: #fff;
+}
 .logo {
   float: left;
   width: 250px;
@@ -21,6 +32,7 @@
   min-width: 1000px;
   width: 100%;
   position: absolute;
+  background: #2d8cf0;
 }
 
 .layout-content {
@@ -28,7 +40,7 @@
   background: #fff;
   border-radius: 4px;
   position: absolute;
-  top: 60px;
+  top: 90px;
   left: 0;
   right: 0;
   bottom: 0;
@@ -40,7 +52,7 @@
 }
 
 .ivu-menu-item {
-  padding: 0 10px;
+  padding: 0 10px !important;
 }
 
 .layout-right-menu {
@@ -68,36 +80,20 @@
 <template>
   <div class="layout">
     <div class="layout-header">
-      <Menu mode="horizontal" theme="primary" :active-name="$route.path" @on-select="menuRouter">
-        <h3 class="logo" style="-webkit-app-region: drag">
-          <router-link to="/index">
-            <Icon type="social-snapchat-outline" style="font-size:30px;"></Icon> 钻起来 永久免费互动平台</router-link>
-        </h3>
-        <div>
-          <Menu-item name="/index">
-            <Icon type="home"></Icon>
-            首页
-          </Menu-item>
-          <Menu-item name="/taskFree">
-            <Icon type="android-people"></Icon>
-            免费任务
-          </Menu-item>
-          <Menu-item name="/taskAuto">
-            <Icon type="social-android"></Icon>
-            自动任务
-          </Menu-item>
-          <Menu-item name="/flow">
-            <Icon type="stats-bars"></Icon>
-            收藏 | 搜索流量
-          </Menu-item>
-          <Menu-item name="/express">
-            <Icon type="android-mail"></Icon>
-            空包单号
-          </Menu-item>
-          <Menu-item name="/user">
-            <Icon type="person"></Icon>
-            会员中心
-          </Menu-item>
+      <div class="layout-top-menu">
+        <Row>
+          <Col span="8" style="-webkit-app-region: drag">
+          <h3 class="logo">
+            <router-link to="/index">
+              <Icon type="social-snapchat-outline" style="font-size:20px;"></Icon> 钻起来 永久免费互动平台</router-link>
+          </h3>
+          </Col>
+          <Col span="8" style="-webkit-app-region: drag">&nbsp;</Col>
+          <Col span="8">
+          <span class="layout-right-tools">
+            <label>用户名：fangxingguang</label>&nbsp;
+            <label>余额：1000</label>
+          </span>
           <div class="layout-right-menu">
             <span @click="sendToMain('winMin')">
               <Icon type="minus"></Icon>
@@ -109,8 +105,39 @@
               <Icon type="android-close"></Icon>
             </span>
           </div>
-        </div>
-      </Menu>
+          </Col>
+        </Row>
+      </div>
+      <div class="layout-main-menu">
+        <Menu mode="horizontal" theme="primary" :active-name="$route.path" @on-select="menuRouter">
+          <div>
+            <Menu-item name="/index">
+              <Icon type="home"></Icon>
+              首页
+            </Menu-item>
+            <Menu-item name="/taskFree">
+              <Icon type="android-people"></Icon>
+              免费任务
+            </Menu-item>
+            <Menu-item name="/taskAuto">
+              <Icon type="social-android"></Icon>
+              自动任务
+            </Menu-item>
+            <Menu-item name="/flow">
+              <Icon type="stats-bars"></Icon>
+              收藏 | 搜索流量
+            </Menu-item>
+            <Menu-item name="/express">
+              <Icon type="android-mail"></Icon>
+              空包单号
+            </Menu-item>
+            <Menu-item name="/user">
+              <Icon type="person"></Icon>
+              会员中心
+            </Menu-item>
+          </div>
+        </Menu>
+      </div>
     </div>
     <div class="layout-content">
       <router-view></router-view>
