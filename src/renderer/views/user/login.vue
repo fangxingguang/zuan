d643dddd1e96204df0d7f6ce3aa02304ac9eb797
fangxingguang <fangxingguang@163.com>
2017-08-01 22:36:52 +0800

    TODO

--- src/renderer/views/user/login.vue
+++ src/renderer/views/user/login.vue
@@ -1,6 +1,6 @@
 <<style scoped>
     .content-card{
-        width: 380px;
+        width: 350px;
         min-height: 305px;
         background-color: #fff;
         position: absolute;
@@ -22,7 +22,7 @@
 <template>
     <div>
         <canvas ref="renderCanvas" class="myCanvas"></canvas>
-        <Card class="content-card" v-if="type=='login'">
+        <Card class="content-card" v-show="type=='login'">
             <p slot="title">
                 <Icon type="person"></Icon>
                 登录
@@ -32,24 +32,24 @@
                 <a href="javascript:void(0)" @click="changeShow('register')">注册 >></a>
             </a>
             <div>
-                <Form ref="formInline" :model="formInline" :rules="ruleInline">
-                    <Form-item prop="user">
-                        <Input type="text" v-model="formInline.user" placeholder="用户名">
+                <Form ref="loginForm" :model="loginForm" :rules="rules">
+                    <Form-item prop="name">
+                        <Input type="text" size="large" v-model="loginForm.name" placeholder="用户名">
                         <Icon type="ios-person-outline" slot="prepend"></Icon>
                         </Input>
                     </Form-item>
                     <Form-item prop="password">
-                        <Input type="password" v-model="formInline.password" placeholder="密码">
+                        <Input type="password" size="large" v-model="loginForm.password" placeholder="密码">
                         <Icon type="ios-locked-outline" slot="prepend"></Icon>
                         </Input>
                     </Form-item>
                     <Form-item>
-                        <Button type="primary" size="large" @click="handleSubmit('formInline')">登录</Button>
+                        <Button type="primary" size="large" @click="loginSubmit('loginForm')">登录</Button>
                     </Form-item>
                 </Form>
             </div>
         </Card>
-        <Card class="content-card" v-if="type=='register'">
+        <Card class="content-card" v-show="type=='register'">
             <p slot="title">
                 <Icon type="person-add"></Icon>
                 注册
@@ -59,24 +59,24 @@
                 <a href="javascript:void(0)" @click="changeShow('login')">登录 >></a>
             </a>
             <div>
-                <Form ref="formInline" :model="formInline" :rules="ruleInline">
-                    <Form-item prop="user">
-                        <Input type="text" v-model="formInline.user" placeholder="用户名">
+                <Form ref="registerForm" :model="registerForm" :rules="rules">
+                    <Form-item prop="name">
+                        <Input type="text" size="large" v-model="registerForm.name" placeholder="用户名">
                         <Icon type="ios-person-outline" slot="prepend"></Icon>
                         </Input>
                     </Form-item>
                     <Form-item prop="password">
-                        <Input type="password" v-model="formInline.password" placeholder="密码">
+                        <Input type="password" size="large" v-model="registerForm.password" placeholder="密码">
                         <Icon type="ios-locked-outline" slot="prepend"></Icon>
                         </Input>
                     </Form-item>
                     <Form-item prop="code">
-                        <Input type="text" v-model="formInline.code" placeholder="验证码" style="width:100px;">
+                        <Input type="text" v-model="registerForm.code" placeholder="验证码" style="width:100px;">
                         <Icon type="image" slot="prepend"></Icon>
                         </Input>
                     </Form-item>
                     <Form-item>
-                        <Button type="primary" size="large" @click="handleSubmit('formInline')">注册</Button>
+                        <Button type="primary" size="large" @click="registerSubmit('registerForm')">注册</Button>
                     </Form-item>
                 </Form>
             </div>
@@ -89,18 +89,26 @@ export default {
     data() {
         return {
             type: 'login',
-            formInline: {
-                user: '',
+            loginForm: {
+                name: '',
                 password: ''
             },
-            ruleInline: {
-                user: [
+            registerForm: {
+                name: '',
+                password: '',
+                code: ''
+            },
+            rules: {
+                name: [
                     { required: true, message: '请填写用户名', trigger: 'blur' }
                 ],
                 password: [
                     { required: true, message: '请填写密码', trigger: 'blur' },
                     { type: 'string', min: 6, message: '密码长度不能小于6位', trigger: 'blur' }
-                ]
+                ],
+                code: [
+                    { required: true, message: '请填写验证码', trigger: 'blur' }
+                ],
             }
         }
     },
@@ -108,12 +116,23 @@ export default {
         this.canvas();
     },
     methods: {
-        handleSubmit(name) {
+        loginSubmit(name) {
             this.$refs[name].validate((valid) => {
                 if (valid) {
-                    this.$Message.success('提交成功!');
-                } else {
-                    this.$Message.error('表单验证失败!');
+                    this.$http.post('login', loginForm).then((response) => {
+                        console.log(response);
+                        this.$Message.success('提交成功!');
+                    });
+                }
+            })
+        },
+        registerSubmit(name) {
+            this.$refs[name].validate((valid) => {
+                if (valid) {
+                    this.$http.post('register', loginForm).then((response) => {
+                        console.log(response);
+                        this.$Message.success('提交成功!');
+                    });
                 }
             })
         },
